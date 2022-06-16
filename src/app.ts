c0f6e306a6a1c606a57fad1126d89f6fdd4eb7f4
Lucas LaRocco <llarocco399@hotmail.com>
2022-06-16 14:07:27 -0500

    df

--- src/app.ts
+++ src/app.ts
@@ -30,6 +30,7 @@ app.use((err: any, req: any, res: any, next: any) => {
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   const buildDir = path.join(__dirname, "client", "build");
+  console.log(buildDir);
 
   app.use(express.static(buildDir));
 
